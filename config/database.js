f60ba354dee69bcc4d286fe3ce2182e712ba4246
RaveHunter05 <paulsotelo97@gmail.com>
2020-04-26 00:14:53 -0600

    asdf

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const Sequelize= require('sequelize')
 
-module.exports= new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host:process.env.DB_HOST,
+module.exports= new Sequelize('bd_epic_landscaping', 'Raven', 'elojodesauron123', {
+    host:'138.197.111.216',
     dialect: 'mysql'
 })
